d7db4cdd5c9178b26188f532616cf3883f09b7bc
Mohamed Isoukrane <isoukrane.mohamed@gmail.com>
2016-07-10 13:26:49 -0700

    Use best practices.

--- app/src/main/java/com/mohamedisoukrane/myappportfolio/MainActivity.java
+++ app/src/main/java/com/mohamedisoukrane/myappportfolio/MainActivity.java
@@ -2,24 +2,44 @@ package com.mohamedisoukrane.myappportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int[] ids = {
+                R.id.popular_movies,
+                R.id.stock_hawk,
+                R.id.build_it_bigger,
+                R.id.go_ubiquitous,
+                R.id.make_your_app_material,
+                R.id.capstone
+        };
+        for(int id : ids) {
+            Button btn = (Button) findViewById(id);
+            btn.setOnClickListener(this);
+        }
     }
 
     /** Called when the user touches the button */
-    public void sendMessage(View view) {
+    public void showToast(View view) {
+        if (toast != null) {
+            toast.cancel();
+        }
         Button button = (Button) view;
         String toastStr = "This button will launch my " + button.getText() + " app!";
-        Log.e("MY_TAG", "we are not here");
-        Toast.makeText(this, toastStr, Toast.LENGTH_SHORT).show();
+        toast = Toast.makeText(this, toastStr, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        showToast(view);
     }
 }
